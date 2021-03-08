2c5ba2be3f0f5621bc8fc654a927224c47c3ad1e
Borja <borja@debian>
2021-03-08 21:03:53 +0100

    he terminado todos los ejercicios del proyecto

--- ejercicios-json.py
+++ ejercicios-json.py
@@ -1,9 +1,30 @@
-from funciones import leer_json, listado_partidos
+from funciones import *
 import sys
 
 fichero = "./proyecto_json/ejercicios_json/partidosfutbol.json"
 partidos = leer_json(fichero)
 
-for i in listado_partidos(partidos):
-    print(i)
+
     
+
+opcion=0
+
+while opcion!=6:
+    if opcion==1:
+        for i in listado_equipos(partidos):
+            print(i)
+    if opcion==2:
+        print(numero_partidos(partidos))
+    if opcion==3:
+        equipo = input("dime el nombre del equipo que quieres buscar: ")
+        liga = input("dime el nombre de la liga ")
+        busqueda = busqueda_partido(partidos,equipo,liga)
+
+    if opcion==4:
+        liga = input("dime el nombre de la liga ")
+        for i in busqueda_liga(partidos,liga):
+            print(i)
+    if opcion==5:
+        equipo = input("dime el nombre del equipo que quieres buscar: ")
+        busqueda = busqueda_url_partido(partidos,equipo)
+    opcion= menu(opcion)
--- funciones.py
+++ funciones.py
@@ -6,12 +6,51 @@ def leer_json(fichero):
         datos=json.load(f)
     return datos
 
-def listado_partidos(partido):
+def listado_equipos(partido):
     res = []
     
     for i in partido.get("timeline"):
-        lista = []
-        lista.append(i.get("localTeam").get("name"))
-        lista.append(i.get("visitorTeam").get("name"))
-        res.append(lista) 
+        res.append(i.get("localTeam").get("name"))
+        res.append(i.get("visitorTeam").get("name")) 
     return res
+
+def numero_partidos(partidos):
+    return len(partidos.get("timeline"))
+
+def busqueda_partido(partidos,equipo,liga):
+    for i in partidos.get("timeline"):
+        if equipo.upper() == i.get("localTeam").get("name").upper():
+            equipo_rival=i.get("visitorTeam").get("name")
+            fecha = i.get("startTime")
+        if equipo.upper() == i.get("visitorTeam").get("name").upper():
+            equipo_rival=i.get("localTeam").get("name")
+            fecha = i.get("startTime")
+    return print ("Juega contra ",equipo_rival,"a las", fecha)
+
+def busqueda_liga(partidos,liga):
+    res =[]
+    for i in partidos.get("timeline"):
+        if liga.upper() == i.get("championship").get("shortName").upper(): 
+            res.append(i.get("localTeam").get("name"))
+            res.append(i.get("visitorTeam").get("name"))
+    return res
+
+def busqueda_url_partido(partidos,equipo):
+    for i in partidos.get("timeline"):
+        if equipo.upper() == i.get("localTeam").get("name").upper():
+            url = i.get("sources").get("url")
+        if equipo.upper() == i.get("visitorTeam").get("name").upper():
+            url = i.get("sources").get("url")
+                 
+    return print ("la url es:",url)
+
+def menu(opcion):
+    print("")
+    print("1- Muestra los nombres de los equipos que participan en las apuestas.")
+    print ("2- Muestrame el numero de partidos que hay registradas.")
+    print("3- Muestra el nombre del equipo rival y la fecha del partido.")
+    print("4- Muestra los nombres que participan en una liga determinada.")
+    print("5- Muestra la url de donde fue emitido el partido del equipo elegido")
+    print("6- Salir.")
+    opcion = int(input("dime la opcion a elegir "))
+    return opcion
